6dc375f5d0ce6fd59c4d9f7c7bebbf2d8f28414b
pl7899 <pl7899@ymail.com>
2025-11-18 18:38:23 -0500

    add debug

--- cardb_interface.php
+++ cardb_interface.php
@@ -5,6 +5,7 @@ $db = db_connect();
 if ($_POST['action'] == "vehicle_initial_button_generation")
 {
 	$rows = mysqli_query($db, "SELECT * FROM `cardb_cars`");
+	echo "trying to parse db";
 	while ($row = mysqli_fetch_array($rows)) 
 	{
 		echo "found : " . $row['vin'] + " make : " + $row['make'];
