4ea8a8876f290d211d98b180f406216afbbc3155
pl7899 <pl7899@ymail.com>
2025-11-18 05:04:24 -0500

    add button add linne break

--- cardb.php
+++ cardb.php
@@ -19,10 +19,16 @@
     width: auto;               /* Maintain aspect ratio */
     object-fit: contain;
   }
+  .lineHorizontal {
+    border-top: 1px solid #000;
+    width: 100%;
+    height: 5px;
+  }	
 </style>
 </head>
 <body>
 <div class="image-container">
+  <button style="width: 200px; height: 150px;">Add Car</button>    
   <img src="https://picsum.photos/400/300" alt="1" />
   <img src="https://picsum.photos/100/150" alt="2" />
   <img src="https://picsum.photos/300/300" alt="3" />
@@ -35,7 +41,7 @@
   <img src="https://picsum.photos/300/700" alt="5" />
   <!-- Add up to 15 images similarly -->
 </div>
-
+<hr class="lineHorizontal">   
 </body>
 </html>
 
