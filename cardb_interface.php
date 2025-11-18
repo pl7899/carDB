b0f92a92c5121e46ad4ae9932a7dce46123257e3
pl7899 <pl7899@ymail.com>
2025-11-18 18:43:25 -0500

    double quote

--- cardb_interface.php
+++ cardb_interface.php
@@ -5,10 +5,10 @@ $db = db_connect();
 if ($_POST['action'] == "vehicle_initial_button_generation")
 {
 	$rows = mysqli_query($db, "SELECT * FROM `cardb_cars`");
-	echo 'trying to parse db';
+	echo "trying to parse db";
 	while ($row = mysqli_fetch_array($rows)) 
 	{
-		echo 'found : ' . $row['vin'] + ' make : ' + $row['make'];
+		echo "found : " . $row['vin'] + " make : " + $row['make'];
 	}
 echo <<<EOT
 		<button style="width: 200px; height: 150px;">Add Car</button>
