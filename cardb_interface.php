a068135ae994790c05d65701f7bc55111468d9fe
pl7899 <pl7899@ymail.com>
2025-11-18 13:34:51 -0500

    experiment with echo output

--- cardb_interface.php
+++ cardb_interface.php
@@ -2,12 +2,20 @@
 
 if ($_POST['action'] == "vehicle_initial_button_generation")
 {
-	echo 'vehicle_initial_button_generation Called';
-
+	echo <<<EOT
+		<button style="width: 200px; height: 150px;">Add Car</button>
+		<img src="https://picsum.photos/400/300" alt="1" />
+		<img src="https://picsum.photos/100/150" alt="2" />
+		<img src="https://picsum.photos/300/300" alt="3" />
+		<img src="https://picsum.photos/200/300" alt="4" />
+		<img src="https://picsum.photos/300/200" alt="5" />
+		<img src="https://picsum.photos/300/200" alt="6" />
+		<img src="https://picsum.photos/400/200" alt="7" />
+		<img src="https://picsum.photos/300/500" alt="8" />
+	EOT;
 }
 elseif ($_POST['action'] == "action_initial_maintenance_display")
 {
-	echo 'action_initial_maintenance_display Called';
 }
 else
 {
