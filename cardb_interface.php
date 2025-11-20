1bc698897a7d46b4fbbe4ef1706a2e940a263bf1
pl7899 <pl7899@ymail.com>
2025-11-20 07:57:19 -0500

    first pass at generating vehicle buttons

--- cardb_interface.php
+++ cardb_interface.php
@@ -4,23 +4,15 @@ $db = db_connect();
 
 if ($_POST['action'] == "vehicle_initial_button_generation")
 {
+	echo "<button style=\"width: 200px; height: 150px;\">Add a Car</button>";
 	$rows = mysqli_query($db, "SELECT * FROM `cardb_cars`");
 	echo "trying to parse db";
 	while ($row = mysqli_fetch_array($rows)) 
 	{
 		echo "found : " . $row['vin'] . " make : " . $row['make'];
+		echo "<button style=\"width: 200px; height: 150px;\">" . $row['vin'] . $row['model'] . "</button>";
 	}
-echo <<<EOT
-		<button style="width: 200px; height: 150px;">Add a Car</button>
-		<img src="https://picsum.photos/400/300" alt="1" />
-		<img src="https://picsum.photos/100/150" alt="2" />
-		<img src="https://picsum.photos/300/300" alt="3" />
-		<img src="https://picsum.photos/200/300" alt="4" />
-		<img src="https://picsum.photos/300/200" alt="5" />
-		<img src="https://picsum.photos/300/200" alt="6" />
-		<img src="https://picsum.photos/400/200" alt="7" />
-		<img src="https://picsum.photos/300/500" alt="8" />
-	EOT;
+
 }
 elseif ($_POST['action'] == "action_initial_maintenance_display")
 {
