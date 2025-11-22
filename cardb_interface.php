b66eab13ff17c81bb801e1046c9ee5b671c2d46a
pl7899 <pl7899@ymail.com>
2025-11-22 04:31:24 -0500

    switch to heredoc

--- cardb_interface.php
+++ cardb_interface.php
@@ -20,32 +20,31 @@ elseif ($_POST['action'] == "action_initial_maintenance_display")
 }
 elseif ($_POST['action'] == "action_add_vehicle")
 {
-	echo <<<EOB
-		<FORM>    <form action="/submit-vehicle" method="POST">
-        <label for="make">Make:</label>
-        <input type="text" id="make" name="make" required><br><br>
+echo <<<EOB
+<FORM>    <form action="/submit-vehicle" method="POST">
+    <label for="make">Make:</label>
+    <input type="text" id="make" name="make" required><br><br>
 
-        <label for="model">Model:</label>
-        <input type="text" id="model" name="model" required><br><br>
+    <label for="model">Model:</label>
+    <input type="text" id="model" name="model" required><br><br>
 
-        <label for="year">Year:</label>
-        <input type="number" id="year" name="year" required><br><br>
+    <label for="year">Year:</label>
+    <input type="number" id="year" name="year" required><br><br>
 
-        <label for="vin">VIN Number:</label>
-        <input type="text" id="vin" name="vin" required><br><br>
+    <label for="vin">VIN Number:</label>
+    <input type="text" id="vin" name="vin" required><br><br>
 
-        <label for="registration">Registration Number:</label>
-        <input type="text" id="registration" name="registration" required><br><br>
+    <label for="registration">Registration Number:</label>
+    <input type="text" id="registration" name="registration" required><br><br>
 
-        <label for="mileage">Mileage:</label>
-        <input type="number" id="mileage" name="mileage" required><br><br>
+    <label for="mileage">Mileage:</label>
+    <input type="number" id="mileage" name="mileage" required><br><br>
 
-        <label for="license">License Plate:</label>
-        <input type="text" id="license" name="license" required><br><br>
-
-        <button type="submit">Submit</button>
-    </form></FORM>
-	>>>EOB;
+    <label for="license">License Plate:</label>
+    <input type="text" id="license" name="license" required><br><br>
+    <button type="submit">Submit</button>
+</form></FORM>
+EOB;
 }
 else
 {
