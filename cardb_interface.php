5c25127b707717349992cabf78d25493649e767b
pl7899 <pl7899@ymail.com>
2025-11-22 04:12:02 -0500

    add vehicle form

--- cardb_interface.php
+++ cardb_interface.php
@@ -10,13 +10,42 @@ if ($_POST['action'] == "vehicle_initial_button_generation")
 	while ($row = mysqli_fetch_array($rows)) 
 	{
 		echo "found : " . $row['vin'] . " make : " . $row['make'];
-		echo "<button style=\"width: 200px; height: 150px;\">" . $row['vin'] . $row['model'] . "</button>";
+		echo "<button style=\"width: 200px; height: 150px;\">" . $row['vin'] . " " . $row['make'] . "</button>";
 	}
 
 }
 elseif ($_POST['action'] == "action_initial_maintenance_display")
 {
 	echo 'action_initial_maintenance_display';
+}
+elseif ($_POST['action'] == "action_add_vehicle")
+{
+	echo <<<EOB
+		<FORM>    <form action="/submit-vehicle" method="POST">
+        <label for="make">Make:</label>
+        <input type="text" id="make" name="make" required><br><br>
+
+        <label for="model">Model:</label>
+        <input type="text" id="model" name="model" required><br><br>
+
+        <label for="year">Year:</label>
+        <input type="number" id="year" name="year" required><br><br>
+
+        <label for="vin">VIN Number:</label>
+        <input type="text" id="vin" name="vin" required><br><br>
+
+        <label for="registration">Registration Number:</label>
+        <input type="text" id="registration" name="registration" required><br><br>
+
+        <label for="mileage">Mileage:</label>
+        <input type="number" id="mileage" name="mileage" required><br><br>
+
+        <label for="license">License Plate:</label>
+        <input type="text" id="license" name="license" required><br><br>
+
+        <button type="submit">Submit</button>
+    </form></FORM>
+	>>>EOB;
 }
 else
 {
--- cardb_shim.js
+++ cardb_shim.js
@@ -13,22 +13,22 @@ function action_initial_maintenance_display() {
         });
 }
 
-function action_add_car() {
-    $.post("cardb_interface.php", { action: "action_initial_maintenance_display" },
+function action_add_vehicle() {
+    $.post("cardb_interface.php", { action: "action_add_vehicle" },
         function(data) {
     		$('#action_output').html(data);
         });
 }
 
 function action_select_car() {
-    $.post("cardb_interface.php", { action: "action_initial_maintenance_display" },
+    $.post("cardb_interface.php", { action: "action_select_car" },
         function(data) {
     		$('#action_output').html(data);
         });
 }
 
 function action_administration() {
-    $.post("cardb_interface.php", { action: "action_initial_maintenance_display" },
+    $.post("cardb_interface.php", { action: "action_administration" },
         function(data) {
     		$('#action_output').html(data);
         });
