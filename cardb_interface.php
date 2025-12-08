f552534b92799a6b68ab46fef7f220d8b98a956a
pl7899 <pl7899@ymail.com>
2025-12-08 18:06:31 -0500

    remove debug from button list

--- cardb_interface.php
+++ cardb_interface.php
@@ -10,10 +10,8 @@ if ($_POST['action'] == "vehicle_initial_button_generation")
 {
 	echo "<button onclick=\"action_add_vehicle()\" style=\"width: 200px; height: 150px;\">Add a Car</button>";
 	$rows = mysqli_query($db, "SELECT * FROM `cardb_cars`");
-	echo "trying to parse db";
 	while ($row = mysqli_fetch_array($rows)) 
 	{
-		echo "found : " . $row['vin'] . " make : " . $row['make'];
 		echo "<button onclick=\"action_select_car(" . $row['vin'] . ")\" style=\"width: 200px; height: 150px;\">" . $row['vin'] . " " . $row['make'] . "</button>";
 	}
 
