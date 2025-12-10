7118207c6e7d223dc096483c3be093aaab2e75bf
pl7899 <pl7899@ymail.com>
2025-12-09 20:06:01 -0500

    quick test

--- cardb_interface.php
+++ cardb_interface.php
@@ -101,25 +101,25 @@ elseif ($_POST['action'] == "modify_existing_vehicle")
 	$row = mysqli_fetch_array($rows);
 	var_dump( $row);
 	echo '<label for="make">Make:</label>';
-    echo '<input type="text" id="make" name="make" required placeholder="' . $row[make] . '" /> <br><br>';
+    echo '<input type="text" id="make" name="make" required placeholder="make" /> <br>';
 
     echo '<label for="model">Model:</label>';
-    echo '<input type="text" id="model" name="model" required placeholder="' . $row[model] . '" /> <br><br>';
+    echo '<input type="text" id="model" name="model" required placeholder="' . $row[model] . '" /> <br>';
 
     echo '<label for="year">Year:</label>';
-    echo '<input type="number" id="year" name="year" required placeholder="' . $row[year] . '" /> <br><br>';
+    echo '<input type="number" id="year" name="year" required placeholder="' . $row[year] . '" /> <br>';
 
     echo '<label for="vin">VIN Number:</label>';
-    echo '<input type="text" id="vin" name="vin" required placeholder="' . $row[vin] . '" /> <br><br>';
+    echo '<input type="text" id="vin" name="vin" required placeholder="' . $row[vin] . '" /> <br>';
 
     echo '<label for="registration">Registration Number:</label>';
-    echo '<input type="text" id="registration" name="registration" required placeholder="' . $row[registration] . '" /> <br><br>';
+    echo '<input type="text" id="registration" name="registration" required placeholder="' . $row[registration] . '" /> <br>';
 
     echo '<label for="mileage">Mileage:</label>';
-    echo '<input type="number" id="mileage" name="mileage" required placeholder="' . $row[miles] . '" /> <br><br>';
+    echo '<input type="number" id="mileage" name="mileage" required placeholder="' . $row[miles] . '" /> <br>';
 
     echo '<label for="license">License Plate:</label>';
-    echo '<input type="text" id="license" name="license" required placeholder="' . $row[model] . '" /> <br><br>';
+    echo '<input type="text" id="license" name="license" required placeholder="' . $row[model] . '" /> <br>';
     echo '<button onclick="updateExistingVehicle();">Modify Car</button>'; 
 }
 elseif ($_POST['action'] == "delete_vehicle")
