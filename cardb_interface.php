78ff0d27f5f1e2b3e5be801af7bf13f566097315
pl7899 <pl7899@ymail.com>
2025-12-22 20:20:57 -0500

    tios

--- cardb_interface.php
+++ cardb_interface.php
@@ -17,7 +17,7 @@ if ($_POST['action'] == "vehicle_initial_button_generation")
 
         // Check if the image URL is valid
         if (!empty($imageUrl)) {
-            echo "<button class=\"button\" onclick=\"setActiveCar(" . $row['id'] . ")\" style=\"width: 250px; height: 200px; background-image: url('" . $imageUrl . "'); background-size: cover; background-position: center;\"></button>";
+            echo "<button class=\"button\" onclick=\"setActiveCar(" . $row['id'] . ")\" title=\"Set" . $row['name'] . " Active\" style=\"width: 250px; height: 200px; background-image: url('" . $imageUrl . "'); background-size: cover; background-position: center;\"></button>";
         } else {
             // Fallback to showing the name if no valid image is available
             echo "<button class=\"button\" onclick=\"setActiveCar(" . $row['id'] . ")\" style=\"width: 250px; height: 200px;\">" . $row['name'] . "</button>";
