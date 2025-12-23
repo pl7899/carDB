69b0818ca95f749d6bb522370107f73f54a3e06c
pl7899 <pl7899@ymail.com>
2025-12-22 20:39:44 -0500

    updated edit vehicle form

--- cardb_interface.php
+++ cardb_interface.php
@@ -101,32 +101,68 @@ elseif ($_POST['action'] == "submit_new_vehicle")
 }
 elseif ($_POST['action'] == "modify_existing_vehicle")
 {
-	$query = "SELECT * FROM `cardb_cars` WHERE `id` = '" . $_POST['activeCar'] . "';";
-	$rows = mysqli_query($db, $query);
-	if($rows == null)
-	{
-		return;
-	}	
-	$row = mysqli_fetch_array($rows);
-	echo '<label for="name">Name:</label>';
-    echo '<input type="text" id="name" name="name" required value="' . $row['name'] . '" /> <br>';
-	echo '<label for="make">Make:</label>';
-    echo '<input type="text" id="make" name="make" required value="' . $row['make'] . '" /> <br>';
-    echo '<label for="model">Model:</label>';
-    echo '<input type="text" id="model" name="model" required value="' . $row['model'] . '" /> <br>';
-    echo '<label for="year">Year:</label>';
-    echo '<input type="number" id="year" name="year" required value="' . $row['year'] . '" /> <br>';
-	echo '<label for="year">image:</label>';
-    echo '<input type="text" id="image" name="image" required value="' . $row['image'] . '" /> <br>';
-    echo '<label for="vin">VIN Number:</label>';
-    echo '<input type="text" id="vin" name="vin" required value="' . $row['vin'] . '" /> <br>';
-    echo '<label for="registration">Registration Number:</label>';
-    echo '<input type="text" id="registration" name="registration" required value="' . $row['registration'] . '" /> <br>';
-    echo '<label for="mileage">Mileage:</label>';
-    echo '<input type="number" id="mileage" name="mileage" required value="' . $row['miles'] . '" /> <br>';
-    echo '<label for="license">License Plate:</label>';
-    echo '<input type="text" id="license" name="license" required value="' . $row['plate'] . '" /> <br>';
-    echo '<button class=\"button\" onclick="pushVehicleUpdates(' . $_POST['activeCar'] . ');">Modify Car</button>'; 
+    $query = "SELECT * FROM `cardb_cars` WHERE `id` = '" . $_POST['activeCar'] . "';";
+    $rows = mysqli_query($db, $query);
+    if ($rows == null) {
+        return;
+    }
+    $row = mysqli_fetch_array($rows);
+
+    echo '<h2>Modify Vehicle Information</h2>';
+    echo '<form id="modifyVehicleForm">';
+
+    // Use a table for better alignment of labels and input fields
+    echo '<table class="form-table">';
+    echo '<tr>';
+    echo '<td><label for="name">Name:</label></td>';
+    echo '<td><input type="text" id="name" name="name" required value="' . $row['name'] . '" /></td>';
+    echo '</tr>';
+
+    echo '<tr>';
+    echo '<td><label for="make">Make:</label></td>';
+    echo '<td><input type="text" id="make" name="make" required value="' . $row['make'] . '" /></td>';
+    echo '</tr>';
+
+    echo '<tr>';
+    echo '<td><label for="model">Model:</label></td>';
+    echo '<td><input type="text" id="model" name="model" required value="' . $row['model'] . '" /></td>';
+    echo '</tr>';
+
+    echo '<tr>';
+    echo '<td><label for="year">Year:</label></td>';
+    echo '<td><input type="number" id="year" name="year" required value="' . $row['year'] . '" /></td>';
+    echo '</tr>';
+
+    echo '<tr>';
+    echo '<td><label for="image">Image URL:</label></td>';
+    echo '<td><input type="text" id="image" name="image" required value="' . $row['image'] . '" /></td>';
+    echo '</tr>';
+
+    echo '<tr>';
+    echo '<td><label for="vin">VIN Number:</label></td>';
+    echo '<td><input type="text" id="vin" name="vin" required value="' . $row['vin'] . '" /></td>';
+    echo '</tr>';
+
+    echo '<tr>';
+    echo '<td><label for="registration">Registration Number:</label></td>';
+    echo '<td><input type="text" id="registration" name="registration" required value="' . $row['registration'] . '" /></td>';
+    echo '</tr>';
+
+    echo '<tr>';
+    echo '<td><label for="mileage">Mileage:</label></td>';
+    echo '<td><input type="number" id="mileage" name="mileage" required value="' . $row['miles'] . '" /></td>';
+    echo '</tr>';
+
+    echo '<tr>';
+    echo '<td><label for="license">License Plate:</label></td>';
+    echo '<td><input type="text" id="license" name="license" required value="' . $row['plate'] . '" /></td>';
+    echo '</tr>';
+    echo '</table>';
+
+    // Add a submit button
+    echo '<br>';
+    echo '<button class="button" onclick="pushVehicleUpdates(' . $_POST['activeCar'] . ');">Modify Car</button>';
+    echo '</form>';
 }
 elseif ($_POST['action'] == "show_new_maintenance_entry")
 {
