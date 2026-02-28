f9c603bdf0d9bb73bee80c34378dfe2250c52a79
pl7899 <pl7899@ymail.com>
2026-02-27 20:32:19 -0500

    typo fix

--- cardb_interface_json.php
+++ cardb_interface_json.php
@@ -71,8 +71,8 @@ elseif ($_POST['action'] == "json_retrieve_all_maint")
 }
 elseif ($_POST['action'] == "json_select_car")
 {
-	echo "json_select_car entry -- active_car = " . $_POST['active_car'];
-	$query = "SELECT * FROM `cardb_cars` WHERE `id` = '" . $_POST['active_Car'] . "';";
+	echo "json_select_car entry -- active_car = " . $_POST['activeCar'];
+	$query = "SELECT * FROM `cardb_cars` WHERE `id` = '" . $_POST['activeCar'] . "';";
 	$rows = mysqli_query($db, $query);
 	if($rows == null)
 	{
